9e2de580c2bdd5a4b978f517e139ff4f04e7c6c0
Samet Cavur <samet.cavur@etiya.com>
2022-02-06 19:22:15 +0300

    Data Structure and Algorithms

---
+++ src/LinkedList/DoubleWayLinearList/Node.java
@@ -0,0 +1,13 @@
+package LinkedList.DoubleWayLinearList;
+
+public class Node {
+	public int number;
+	public Node nextNode;
+	public Node prevNode;
+	
+	public Node(int number) {
+		this.number = number;
+		this.nextNode = null;
+		this.prevNode = null;
+	}
+}
---
+++ src/LinkedList/OneWayLinearList/Node.java
@@ -0,0 +1,7 @@
+package LinkedList.OneWayLinearList;
+
+public class Node {
+	public int number;
+	public Node nextNode;
+
+}
